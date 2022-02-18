9451121c5b36b0307d206a95605f1ca474b6f9ed
Renata-CDB <renatabianchine2016@gmail.com>
2022-02-18 16:20:42 -0300

    third commit

--- src/main/java/controller/NameOwnerController.java
+++ src/main/java/controller/NameOwnerController.java
@@ -3,8 +3,13 @@ package controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.NameOwner;
@@ -13,11 +18,20 @@ import model.NameOwner;
 @RequestMapping("/NameOwner")
 public class NameOwnerController 
 {   
+	@Autowired
+	private repository.NameOwnerRepository NameOwnerRepository;
 	
 	@GetMapping
 	public List<NameOwner>listar() 
 	{
-		
+		return NameOwnerRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public NameOwner add(@RequestBody NameOwner nameOwner) 
+	{
+		return NameOwnerRepository.save(nameOwner);
 	}
 	
 	
--- src/main/java/repository/NameOwnerRepository.java
+++ src/main/java/repository/NameOwnerRepository.java
@@ -1,10 +1,13 @@
 package repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import model.NameOwner;
 
+@Repository
 public interface NameOwnerRepository extends JpaRepository<NameOwner, Long>
 {
+	
 
 }
